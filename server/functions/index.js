b9e3bcaa7b5d20ec0f52c718f1f709e56c935631
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2021-02-23 14:42:26 -0800

    server

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,11 +1,30 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 var cors = require("cors")
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var info = require('./things.json')
+
+var client_id = info.id; // Your client id
+var client_secret = info.secret; // Your secret
+var redirect_uri = 'http://localhost:5000/spotify-yellow-282e0/us-central1/api/callback'; // Your redirect uri
 
 const express = require('express');
 const app = express();
-app.use(cors())
+app.use(cors()).use(cookieParser())
 
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+var stateKey = 'spotify_auth_state';
 
 app.get('/login', function(req, res) {
 
@@ -14,14 +33,15 @@ app.get('/login', function(req, res) {
   
     // your application requests authorization
     var scope = 'user-read-private user-read-email user-read-playback-state';
+
     res.redirect('https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state
-      }));
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
   });
   
   app.get('/callback', function(req, res) {
